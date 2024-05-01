695d9f9730b824db202dd12ebcdc0d053d718684
Adisa Mrvoljak <adisamrvoljak@gmail.com>
2024-05-01 23:32:07 +0200

    add winston logger

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
-import fs from "fs";
+import fs from 'fs';
+import winston from 'winston';
+import logger from './logger';
 
-console.log("Hello");
+console.log('Hello');
+
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV !== 'production') {
+    logger.add(new winston.transports.Console());
+}
+
+logger.info('Log info');
+logger.error('Log error');
